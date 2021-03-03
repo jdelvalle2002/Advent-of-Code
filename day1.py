fcb02cfc363603d5be29dcd389666f37eac0571e
jdelvalle2002 <63021540+jdelvalle2002@users.noreply.github.com>
2021-03-03 01:18:43 -0300

    day1

---
+++ day1.py
@@ -0,0 +1,14 @@
+l = open("inputd1.txt")
+lista = l.readlines()
+lista = list(map(lambda x: x.strip(), lista))
+new_l = list(map(int, lista))
+
+#print(new_l)
+for i in new_l:
+    for j in new_l:
+        if i + j == 2020:
+            resu = i*j
+            break
+print("############")
+print(resu)
+print("@@@@@@@@@@@@")
